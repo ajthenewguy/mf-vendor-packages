63d5041ad9c3caaaca382b3b80a493a4948568ae
Allen McCabe <allenmccabe@gmail.com>
2016-03-06 09:02:05 -0800

    Update class autoloader class mapping

--- composer/autoload_psr4.php
+++ composer/autoload_psr4.php
@@ -15,7 +15,7 @@ return array(
     'Seld\\PharUtils\\' => array($vendorDir . '/seld/phar-utils/src'),
     'Seld\\JsonLint\\' => array($vendorDir . '/seld/jsonlint/src/Seld/JsonLint'),
     'Seld\\CliPrompt\\' => array($vendorDir . '/seld/cli-prompt/src'),
-    'MVCFAM\\App\\' => array($vendorDir . '/app/src'),
+    'MVCFAM\\App\\' => array($baseDir . '/../app/src'),
     'JsonSchema\\' => array($vendorDir . '/justinrainbow/json-schema/src/JsonSchema'),
     'Composer\\Spdx\\' => array($vendorDir . '/composer/spdx-licenses/src'),
     'Composer\\Semver\\' => array($vendorDir . '/composer/semver/src'),
